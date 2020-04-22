5fbe645dcef48d42fc1cea1e513748e635fc26c4
tasha0511 <wijesharma@gmail.com>
2020-04-22 18:54:51 +0700

    file .js

---
+++ nodejs/latihan_module4/mysql-create-table-product.js
@@ -0,0 +1,28 @@
+﻿
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "ecommerce"
+});
+
+connection.connect(function (err) {
+    if (err) {
+        throw err;
+    }
+
+    console.log("Terkoneksi dengan sukses!");
+
+    var sql = "CREATE TABLE product(id_product INT(11), nama_product VARCHAR(60), gambar_product VARCHAR(200), harga_product INT(130), des_product TEXT, createdate DATE, PRIMARY KEY(id_product))";
+    connection.query(sql, function (err, result) {
+        if (err) {
+            throw err;
+        }
+
+        console.log("Tabel berhasil dibuat!");
+        connection.destroy();
+
+    });
+});
---
+++ nodejs/latihan_module4/mysql-insert-product.js
@@ -0,0 +1,32 @@
+﻿
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "ecommerce"
+});
+
+connection.connect(function (err) {
+    if (err) {
+        throw err;
+    }
+
+    console.log("Terkoneksi dengan sukses!");
+    
+    var sql = "INSERT INTO product(id_product, nama_product, gambar_product, harga_product, des_product, createdate) VALUES ?";
+    var values = [
+        ['1', 'sepatu sneaker', 'sneaker.jpg', '200000', 'kami menjual sepatu berkualitas semua ukuran', '2020-01-17'],
+        ['2', 'sepatu pantofel', 'pantofel.png', '500000', 'kami menjual sepatu pantofel', '2020-01-30']
+    ];
+    connection.query(sql, [values], function (err, result) {
+        if (err) {
+            throw err;
+        }
+
+        console.log("Number of records inserted: " + result.affectedRows);
+        connection.destroy();
+
+    });
+});
