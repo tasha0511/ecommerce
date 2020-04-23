778598a4253531bb981321934339444d0dedf646
tasha0511 <wijesharma@gmail.com>
2020-04-23 17:39:11 +0700

    exercise module 5 *.js

---
+++ nodejs/latihan_module5/exercise-module5.js
@@ -0,0 +1,19 @@
+﻿
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send('Ini adalah Halaman Home');
+});
+
+app.get('/product', function (req, res) {
+    res.send('Ini adalah Halaman Produk');
+});
+
+app.get('/detailproduct', function (req, res) {
+    res.send('Ini adalah Halaman Detail Produk Toko Anda');
+});
+
+app.listen(8000, function() {
+    console.log('aplikasi ini berjalan pada port 8000!');
+});
