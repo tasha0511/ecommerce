16c7ec8ec794d6b495b1e9483eab9b2470df0605
tasha0511 <wijesharma@gmail.com>
2020-05-14 15:12:50 +0700

    commit pertama

---
+++ exercise6_ecommerce/index.js
@@ -0,0 +1,39 @@
+﻿
+var ecommerce = require('express');
+var app = ecommerce();
+var logger = require('morgan');
+
+
+var ecommerceku = require('./routes/ecommerceku');
+
+var conn = require('express-myconnection');
+var mysql = require('mysql');
+
+app.set('port', process.env.port || 3000);
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use('/public',ecommerce.static(__dirname + '/public'));
+
+app.use(
+    conn(mysql, {
+        host: '127.0.0.1',
+        user: 'root',
+        password: '',
+        database: 'ecommerce_product'
+    }, 'single')
+);
+
+
+app.get('/', function (req, res) {
+    res.send('Server is running on port ' + app.get('port'));
+});
+
+app.get('/exercise6_ecommerce', ecommerceku.home);
+app.get('/exercise6_ecommerce/index', ecommerceku.home);
+app.get('/exercise6_ecommerce/products', ecommerceku.products);
+app.get('/exercise6_ecommerce/single/:id_product', ecommerceku.products_details);
+
+app.listen(app.get('port'), function () {
+    console.log('Server is running on port ' + app.get('port'));
+});
---
+++ exercise6_ecommerce/routes/ecommerceku.js
@@ -0,0 +1,46 @@
+﻿
+exports.home = function (req, res) {
+    req.getConnection(function (err, connect) {
+        var query = connect.query('select * from product', function (err, rows) {
+            if (err) {
+                console.log('Error Message: %', err);
+            }
+
+            res.render('index', {
+                page_title: "Ecommerce Luxury Watch",
+                data: rows
+            });
+        });
+    });
+}
+
+exports.products = function (req, res) {
+    req.getConnection(function (err, connect) {
+        var query = connect.query('select * from product', function (err, rows) {
+            if (err) {
+                console.log('Error Message: %', err);
+            }
+
+            res.render('products', {
+                page_title: "Ecommerce Luxury Watch - Products",
+                data: rows
+            });
+        });
+    });
+}
+
+exports.products_details = function (req, res) {
+    var id_product = req.params.id_product;
+    req.getConnection(function (err, connect) {
+        var query = connect.query('select * from product where id_product=?', id_product, function (err, rows) {
+            if (err) {
+                console.log('Error Message: %', err);
+            }
+
+            res.render('single', {
+                page_title: "Ecommerce Luxury Watch - Product Details",
+                data: rows
+            });
+        });
+    });
+}
